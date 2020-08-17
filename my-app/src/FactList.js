b31e6daf48482f9a34a53b7c3f603dff7887eb02
victorlobocoding <68975526+victorlobocoding@users.noreply.github.com>
2020-08-17 12:01:49 -0400

    Update FactList.js
    formated

--- my-app/src/FactList.js
+++ my-app/src/FactList.js
@@ -3,15 +3,13 @@ import "./App.css";
 
 export const FactList = ({ catData }) => {
   //Figure out how to make a list of facts here
-  
 
   return (
     <div>
       <h2>Fact list goes here:</h2>
-      {
-      catData.map(function (catDataFacts) {
-	return <li>  {catDataFacts.text}  </li>
-})}
+      {catData.map(function (catDataFacts) {
+        return <li> {catDataFacts.text} </li>;
+      })}
     </div>
   );
 };
