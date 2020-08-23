2127bdde9ceafec889cf530c3523a7429f60f09c
victorlobocoding <68975526+victorlobocoding@users.noreply.github.com>
2020-08-22 20:12:09 -0400

    wow

--- src/RandomNumber.js
+++ src/RandomNumber.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function getRandomInt(min, max) {
@@ -23,6 +23,6 @@ export const RandomCatFact = ({ catData }) => {
       >
         Grab Fact
       </button>
-         </>
+    </>
   );
 };
